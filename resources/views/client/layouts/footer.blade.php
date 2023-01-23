ce5bf4ca167f0047e6066aaaab3957711e45dd8a
trinhson7122 <son07012002@gmail.com>
2023-01-23 15:25:47 +0700

    add footer

---
+++ resources/views/client/layouts/footer.blade.php
@@ -0,0 +1,13 @@
+<!-- Footer Start -->
+<footer>
+    <hr>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <script>document.write((new Date).getFullYear());</script>
+                 © TXSon
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- end Footer -->
