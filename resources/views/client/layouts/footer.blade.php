ac57c2f80ec2f5ef4da6181385721688773e2793
trinhson7122 <son07012002@gmail.com>
2023-02-07 19:35:55 +0700

    update footer client

--- resources/views/client/layouts/footer.blade.php
+++ resources/views/client/layouts/footer.blade.php
@@ -1,11 +1,44 @@
 <!-- Footer Start -->
 <footer>
     <hr>
+    <div class="container-fuild ml-3 mr-3">
+        <div class="row row-cols-1 row-cols-2 row-cols-lg-4">
+            <div class="col">
+                <div class="img-fuild">
+                    <a href="/"><img src="{{ asset('images/logo.png') }}" alt="" width="150"></a>
+                </div>
+            </div>
+            <div class="col">
+                <h3 class="text-dark">Author Trịnh Xuân Sơn</h3>
+                <div><i class="uil-location-point"></i> Nguyên Xá, Minh Khai, Bắc Từ Liêm, Hà Nội</div>
+                <div><i class="uil-phone-alt"></i> 0372238783</div>
+                <div><i class="uil-envelope"></i> sonit7122@gmail.com</div>
+                <div><i class="uil-github-alt"></i> github.com/trinhson7122</div>
+                <div><a class="text-secondary" target="_blank" href="https://www.facebook.com/Sondznhe.22/"><i class=" uil-facebook-messenger"></i> Contact me</a></div>
+            </div>
+            <div class="col">
+                <h3 class="text-dark">Về chúng tôi</h3>
+                <div>Giới thiệu</div>
+                <div>Tin tức</div>
+                <div>Khuyến mãi</div>
+                <div>Thành viên</div>
+            </div>
+            <div class="col">
+                <h3 class="text-dark">Chính sách</h3>
+                <div>Cách thức thanh toán</div>
+                <div>Vận chuyển</div>
+                <div>Khuyến mãi</div>
+                <div>Thành viên</div>
+            </div>
+          </div>
+    </div>
+    <hr>
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-md-12 text-center">
+                Coppy right
                 <script>document.write((new Date).getFullYear());</script>
-                 © TXSon
+                ©TrinhXuanSon
             </div>
         </div>
     </div>
